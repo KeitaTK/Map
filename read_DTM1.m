dfbecd560b6caa2a0297336db3509b3e75e926a2
{KeitaTK} <{keita17101042@gmail.com}>
2025-05-18 16:52:28 +0900

    パスの変更

--- read_DTM1.m
+++ read_DTM1.m
@@ -1,6 +1,6 @@
 % DTMを読み込んで、2Dで表示
 % DTMファイルのパスを指定
-dtm_file = 'path/to/odm_dem/dtm.tif';
+dtm_file = 'C:\Users\keita\Documents\local\Map\-2025-5-15-dtm.tif';
 
 % ファイル読み込み
 try
@@ -15,26 +15,26 @@ end
 dtm_data = double(dtm_data);
 
 
-// % 2D表示（上から見た図）
-// figure(1);
-// imagesc(dtm_data);
-// colorbar;
-// title('DTM 2D View');
-// colormap(jet); % お好みのカラーマップを選択
-
-% 3D表示用のグリッド作成
-[rows, cols] = size(dtm_data);
-[X, Y] = meshgrid(1:cols, 1:rows);
-
-% 3D表示
-figure(2);
-surf(X, Y, dtm_data);
-title('DTM 3D View');
-xlabel('X');
-ylabel('Y');
-zlabel('Elevation (m)');
+% 2D表示（上から見た図）
+figure(1);
+imagesc(dtm_data);
 colorbar;
-shading interp; % 滑らかな表示（オプション）
+title('DTM 2D View');
+colormap(jet); % お好みのカラーマップを選択
 
-% 視点を調整
-view(45, 30); % 方位角、仰角
+##% 3D表示用のグリッド作成
+##[rows, cols] = size(dtm_data);
+##[X, Y] = meshgrid(1:cols, 1:rows);
+##
+##% 3D表示
+##figure(2);
+##surf(X, Y, dtm_data);
+##title('DTM 3D View');
+##xlabel('X');
+##ylabel('Y');
+##zlabel('Elevation (m)');
+##colorbar;
+##shading interp; % 滑らかな表示（オプション）
+##
+##% 視点を調整
+##view(45, 30); % 方位角、仰角
